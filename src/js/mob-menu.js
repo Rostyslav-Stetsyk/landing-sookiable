8e5f0f7a80f460b1bc5455ada3c8e7904b475405
Rostyslav-Stetsyk <rostyslav.stetsyk1999@gmail.com>
2023-12-16 17:32:32 +0200

    animation nav

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,17 +1,18 @@
 const refs = {
   button: document.querySelector("#mob-menu"),
-  menu: document.querySelector("#menu"),
   header: document.querySelector(".header.section"),
 };
 
 const openMobMenu = (e) => {
   if (e.currentTarget.ariaExpanded === "false") {
     refs.header.classList.add("js-open-menu");
-    refs.menu.classList.remove("hidden-visually");
+    document.body.style.overflow = "hidden";
+
     e.currentTarget.ariaExpanded = true;
   } else {
     refs.header.classList.remove("js-open-menu");
-    refs.menu.classList.add("hidden-visually");
+    document.body.style.overflow = "auto";
+
     e.currentTarget.ariaExpanded = false;
   }
 };
