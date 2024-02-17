290502d98f2256d8529c209fe42ec172f70a0127
Rostyslav-Stetsyk <rostyslav.stetsyk1999@gmail.com>
2024-02-17 20:50:33 +0200

    delete vite optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,5 @@
 import { defineConfig } from "vite";
 import injectHTML from "vite-plugin-html-inject";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
-
-const DEFAULT_OPTIONS = {
-  test: /\.(jpe?g|png|gif|tiff|webp|avif)$/i,
-  exclude: undefined,
-  include: undefined,
-  includePublic: true,
-  logStats: true,
-  ansiColors: true,
-  png: {
-    quality: 90,
-  },
-  jpeg: {
-    quality: 90,
-  },
-  jpg: {
-    quality: 90,
-  },
-  gif: {},
-  webp: {
-    lossless: true,
-  },
-  avif: {
-    lossless: true,
-  },
-  cache: false,
-  cacheLocation: undefined,
-};
 
 // const debugInsertHTML = {
 //       debug: {
@@ -39,7 +11,7 @@ export default defineConfig({
   root: "./src",
   base: "/landing-sookiable",
   publicDir: "../public",
-  plugins: [injectHTML(), ViteImageOptimizer(DEFAULT_OPTIONS)],
+  plugins: [injectHTML()],
   build: {
     outDir: "../dist",
   },
