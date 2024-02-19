7d83bdab2952a34a601acce163898c9be8224b88
Rostyslav-Stetsyk <rostyslav.stetsyk1999@gmail.com>
2024-02-19 12:46:28 +0200

    fix function open/close mob-menu

--- src/ts/mob-menu.ts
+++ src/ts/mob-menu.ts
@@ -1,11 +1,12 @@
 const refs = {
-  button: document.querySelector("#mob-menu"),
-  header: document.querySelector(".header.section"),
+  button: document.querySelector("#mob-menu") as HTMLElement,
+  header: document.querySelector(".header.section") as HTMLElement,
+  navLinks: document.querySelectorAll(".nav-link") as NodeListOf<HTMLElement>,
 };
 
 const openMobMenu = (e: MouseEvent): void => {
   const target = e.currentTarget as HTMLElement;
-  if (!target || !refs.header) return;
+  if (window.matchMedia("(min-width: 1024px)").matches) return;
 
   if (target.ariaExpanded === "false") {
     refs.header.classList.add("js-open-menu");
@@ -16,20 +17,22 @@ const openMobMenu = (e: MouseEvent): void => {
   } else {
     refs.header.classList.remove("js-open-menu");
     document.body.style.overflow = "auto";
-
-    target.ariaExpanded = "false";
+    refs.button.ariaExpanded = "false";
   }
 };
 
 window
   .matchMedia("(min-width: 1024px)")
   .addEventListener("change", (e): void => {
-    if (!e.matches || !refs.header || !refs.button) return;
+    if (!e.matches) return;
 
     refs.header.classList.remove("js-open-menu");
     document.body.style.overflow = "auto";
-
     refs.button.ariaExpanded = "false";
   });
 
-(refs.button as HTMLElement).addEventListener("click", openMobMenu);
+refs.button.addEventListener("click", openMobMenu);
+
+refs.navLinks.forEach((el) => {
+  el.addEventListener("click", openMobMenu);
+});
