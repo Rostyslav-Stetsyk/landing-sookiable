3efe351b32fb8ac848dbefa1a9acd98fc4fbddef
Rostyslav-Stetsyk <rostyslav.stetsyk1999@gmail.com>
2024-03-03 16:39:53 +0200

    add intl-tel-input

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,12 @@
 import "./mob-menu.ts";
 import "./animated.ts";
 import "./materials.ts";
+import "intl-tel-input/build/js/utils.js";
+
+import intlTelInput from "intl-tel-input";
+
+const input = document.querySelector("#phone") as HTMLElement;
+
+intlTelInput(input, {
+  initialCountry: "UA",
+});
